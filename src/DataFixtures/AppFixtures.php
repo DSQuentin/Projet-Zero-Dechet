1507028772c4d9a37a46851b2d0ae9570cb912b0
Quentin <dsquentin@hotmail.fr>
2021-02-20 18:04:35 +0100

    add responsive design home page, add footer

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,7 +4,6 @@ namespace App\DataFixtures;
 
 use App\Entity\Annonces;
 use App\Entity\User;
-use App\Repository\AnnoncesRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -18,13 +17,22 @@ class AppFixtures extends Fixture
         $today = new \DateTime('NOW');
         $today->format('d/m/Y');
 
-
         for ($i = 0; $i<30; $i++){
+            $author = new User();
+            $author->setUsername("Auteur " . $i)
+                ->setEmail("auteur" . $i . "@mail.com")
+                ->setPassword("testtest")
+                ->setProfilePic('img/default_profile_pic.jpeg')
+                ->setRoles(["ROLE_USER"]);
+            $manager->persist($author);
+
             $annonce = new Annonces();
             $annonce->setTitle('Titre ' . $i)
                 ->setContent("Contenu de l'annonce numéro " . $i . " qui est un peu long quand meme pour faire des tests")
-                ->setCreatedAt($today);
+                ->setCreatedAt($today)
+                ->setAuthor($author);
             $manager->persist($annonce);
+
         }
         $manager->flush();
     }
